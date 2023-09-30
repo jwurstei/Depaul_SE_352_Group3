ee10668b2591a7640e3c5c5048e9f03bb995d128
ayum13 <andrewyum15@gmail.com>
2023-09-29 23:07:20 -0500

    Vehicle Package and H2
    Set up H2 db with seeded data.
    
    Created:
    Vehicle Class
    Vehicle Repository

---
+++ Group3/src/main/java/Group3/demo/vehicles/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Demonstrate
+ * <ul>
+ * <li>Lombok Usage in Vehicle</li>
+ * <li>Spring Data JPA Usage in VehicleRepository</li>
+ * <li>Collection usage in VehicleRepository>
+ * </ul>
+ * 
+ * @Author Andrew Yum
+ */
+package Group3.demo.vehicles;
---
+++ Group3/target/classes/data.sql
@@ -0,0 +1,2 @@
+insert into Vehicles (veh_make, veh_model, veh_year) values ('Honda', 'Civic', 2015);
+insert into Vehicles (veh_make, veh_model, veh_year) values ('Ford', 'Explorer', 2023);
