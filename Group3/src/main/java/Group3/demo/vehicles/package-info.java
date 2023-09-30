577354bd87cae3cf24aae5c0c89bead5a9bede53
ayum13 <andrewyum15@gmail.com>
2023-09-29 23:35:37 -0500

    Update package-info.java

--- Group3/src/main/java/Group3/demo/vehicles/package-info.java
+++ Group3/src/main/java/Group3/demo/vehicles/package-info.java
@@ -1,6 +1,7 @@
 /**
- * Demonstrate
+ * Stuff
  * <ul>
+ * <li>Configured H2 Database</li>
  * <li>Lombok Usage in Vehicle</li>
  * <li>Spring Data JPA Usage in VehicleRepository</li>
  * <li>Collection usage in VehicleRepository>
