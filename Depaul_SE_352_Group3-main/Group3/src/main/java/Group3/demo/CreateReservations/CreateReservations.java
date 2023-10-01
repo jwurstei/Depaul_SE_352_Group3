262cdfaa816493c71e26ea0f85d0814b13b65359
Jake Wursteisen <jwurstei@depaul.edu>
2023-10-01 13:53:24 -0400

    Add files via upload

---
+++ Depaul_SE_352_Group3-main/Group3/src/main/java/Group3/demo/CreateReservations/CreateReservations.java
@@ -0,0 +1,5 @@
+package Group3.demo.CreateReservations;
+
+public class CreateReservations {
+    
+}
--- Depaul_SE_352_Group3-main/Group3/target/classes/data.sql
+++ Depaul_SE_352_Group3-main/Group3/target/classes/data.sql
@@ -1,2 +1,5 @@
 insert into Vehicles (veh_make, veh_model, veh_year) values ('Honda', 'Civic', 2015);
 insert into Vehicles (veh_make, veh_model, veh_year) values ('Ford', 'Explorer', 2023);
+insert into Customers (username, email, password) values ('John Doe', 'john.doe@email.com', 'password1');
+insert into Customers (username, email, password) values ('Jane Doe', 'jane.doe@email.com', 'password2');
+insert into Customers (username, email, password) values ('Drew Brees', 'drew.brees@email.com', 'password3');
