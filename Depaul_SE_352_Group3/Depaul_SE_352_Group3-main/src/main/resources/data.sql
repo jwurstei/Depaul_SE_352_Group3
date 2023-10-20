4ee1a72be83e6d22ad315988fc57c03fff511395
Jake Wursteisen <jwurstei@depaul.edu>
2023-10-19 21:11:58 -0400

    Delete Depaul_SE_352_Group3 directory

--- Depaul_SE_352_Group3/Depaul_SE_352_Group3-main/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into Vehicles (veh_make, veh_model, veh_year) values ('Honda', 'Civic', 2015);
-insert into Vehicles (veh_make, veh_model, veh_year) values ('Ford', 'Explorer', 2023);
--- Depaul_SE_352_Group3/README.md
+++
@@ -1,5 +0,0 @@
-# Depaul_SE_352_Group3
-
-Andrew Yum - Major: CS Year: Senior<br />
-Jake Wursteisen - Major: CS <br />
-Sandeep Turpatti - Major: CS
