55b5b7a30840295af349eb479fb5f7765d22ce5e
Jake Wursteisen <jwurstei@depaul.edu>
2023-10-25 09:53:15 -0400

    Add files via upload

---
+++ src/main/java/Group3/demo/User/dto/SearchDto.java
@@ -0,0 +1,17 @@
+package Group3.demo.User.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class SearchDto {
+    // This class represents a Data Transfer Object (DTO) used for transferring search-related data.
+
+    private String searchTerm;
+    // A field to hold the search term or query provided for searching.
+}
