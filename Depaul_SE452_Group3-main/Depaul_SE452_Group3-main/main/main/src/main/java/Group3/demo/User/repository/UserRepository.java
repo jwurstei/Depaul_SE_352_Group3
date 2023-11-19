411d850f460941d1af481f782178e13b13a35f98
Jake Wursteisen <jwurstei@depaul.edu>
2023-11-19 17:46:05 -0500

    Delete Depaul_SE452_Group3-main/Depaul_SE452_Group3-main/main/main directory

--- Depaul_SE452_Group3-main/Depaul_SE452_Group3-main/main/main/src/main/java/Group3/demo/User/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package Group3.demo.User.repository;
-
-import Group3.demo.User.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User , Long>{
-    User findByUserName(String username);
-    User findByEmail(String email);
-}
--- Depaul_SE452_Group3-main/Depaul_SE452_Group3-main/main/main/src/main/java/Group3/demo/reservations/package-info1.java
+++
@@ -1,7 +0,0 @@
-package Group3.demo.reservations;
-/**
- * </ul>
- * added service layer for reservations
- * updated code with Data Transfer Object (DTO)
- * @Author Jake Wursteisen
- */
